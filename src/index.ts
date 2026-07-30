f8388e0f301662f60dabeccf80cc1f6b924ca850
Abdulaziz Ayalew <abdulazizayalewmussa@gmail.com>
2026-07-30 10:44:50 +0300

    chore: remove legacy plain-Node server, fully migrated to NESTJS

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { loadlaptops } from "./readData";
-
-async function main() {
-    console.log("Welcome to CONNECT.");
-    const Laptops = await loadlaptops("./src/datas/Laptops.json");
-    console.log(Laptops);
-}
-
-main();
