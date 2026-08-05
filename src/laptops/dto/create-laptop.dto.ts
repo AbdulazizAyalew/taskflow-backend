f2bd4c315d4058f643ee18e08ad92a95f1e1a70e
Abdulaziz Ayalew <abdulazizayalewmussa@gmail.com>
2026-08-05 12:01:07 +0300

    feat: add class-based DTOs and enable global validation pipe
    Resolves Issue #17. Installed class-validator and class-transformer, decorated CreateLaptopDto and UpdateLaptopDto, and enabled Valid
    ationPipe in main.ts to prevent bad data on POST and PATCH requests.

--- src/laptops/dto/create-laptop.dto.ts
+++ src/laptops/dto/create-laptop.dto.ts
@@ -1,7 +1,27 @@
+import { IsString, IsInt, IsPositive, IsNotEmpty, MinLength, MaxLength } from "class-validator";
+
 export class CreateLaptopDto {
+  @IsInt()
+  @IsPositive()
   id: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(10,{
+    message: 'Description is too short. it must be atleast 10 characters long!'
+  })
   description: string;
+
+  @IsString()
+  @IsNotEmpty()
   brand: string;
+
+
+  @IsInt()
+  @IsPositive()
   ram: number;
+
+  @IsInt()
+  @IsPositive()
   price: number;
 }
