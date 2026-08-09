ef4f7e57dc7623ddbad460633838efda52648357
Abdulaziz Ayalew <abdulazizayalewmussa@gmail.com>
2026-08-09 11:43:54 +0300

    feat(db): configure TypeORM and PostgreSQL connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,28 @@ import { LaptopsModule } from './laptops/laptops.module';
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
     }),
     LaptopsModule,
     UsersModule,
