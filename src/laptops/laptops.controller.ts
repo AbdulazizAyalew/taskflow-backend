6f56ddace703bbb342ed081521081cb4728ed658
AbdulazizAyalew <abdulazizayalewmussa@gmail.com>
2026-09-01 00:35:38 +0300

    feat: set up Redis caching for Get /Laptops (#53)

--- src/laptops/laptops.controller.ts
+++ src/laptops/laptops.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Patch, Post, Delete, UseGuards, Req } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Delete, UseGuards, Req, UseInterceptors } from '@nestjs/common';
 import {LaptopsService} from './laptops.service';
 import { CreateLaptopDto } from './dto/create-laptop.dto';
 import { updateLaptopDto } from './dto/update-laptop.dto';
@@ -7,6 +7,7 @@ import { Public } from 'src/common/decorators/public.decorator';
 import { RolesGuard } from 'src/common/guards/roles.guard';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { UserRole } from 'src/common/enums/user-role.enum';
+import { CacheInterceptor } from '@nestjs/cache-manager';
 
 @Controller('laptops')
 @UseGuards(JwtAuthGuard,RolesGuard)
@@ -14,7 +15,7 @@ export class LaptopsController {
 
   constructor(private readonly laptopsService: LaptopsService) {}
 
-
+  @UseInterceptors(CacheInterceptor)
   @Public()
   @Get()
   getLaptops() {
